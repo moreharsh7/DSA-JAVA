849029309fa4af0ff6ddd10197800eb565ec3f1b
Harshad More <moreharshad7070@gmail.com>
2025-11-25 14:38:23 +0530

    this is the commit for the arrays REverse

---
+++ dsa/arrays/ReverseArray.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class ReverseArray{
+
+    public static void  Reverse(int num[]){
+        int first=0,last=num.length-1;
+       while(first<last){
+        //swap 
+        int temp=num[last];
+        num[last]=num[first];
+        num[first]=temp;
+        first++;
+        last--;
+
+       }
+     }
+
+  
+    public static void main(String[] args) {
+        int num []={1,2,3,4,5,6,7};
+        Reverse(num);
+        for(int i=0;i<num.length;i++){
+            System.out.print(num[i]+" ");
+        }
+       System.out.println();
+        
+     }
+
+    
+    
+}
+
