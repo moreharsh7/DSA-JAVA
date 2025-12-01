22582f5c1de01921dd144e757604fe29873ea926
Harshad More <moreharshad7070@gmail.com>
2025-12-01 14:13:10 +0530

    this is the commit for the arrays REverse

--- dsa/arrays/printSubArrays.java
+++ dsa/arrays/printSubArrays.java
@@ -5,7 +5,7 @@ public class PrintSubArrays {
         for (int start = 0; start < arr.length; start++) {
             for (int end = start; end < arr.length; end++) {
 
-                // Print subarray from start to end...
+                // Print subarray from start to end 
                 System.out.print("[ ");
                 for (int k = start; k <= end; k++) {
                     System.out.print(arr[k] + " ");
