df23f8adea437681a7e7bc434f663002c1fbf22f
Harshad More <moreharshad7070@gmail.com>
2026-03-31 23:43:31 +0530

    new function added

---
+++ dsa/arrays/dsa/arrays/Sortings.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Sortings {
+
+    public static void bubbleSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]<arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+           System.out.print(arr[i]+" ");
+        }
+        
+        
+    }
+
+
+    public static void main(String[] args) {
+
+        int arr[]={3,6,2,1,8,7,4,5,3,1};
+        bubbleSort(arr);
+        printArr(arr);
+        Scanner sc=new Scanner(System.in);
+
+    }
+
+
+
+    
+}
